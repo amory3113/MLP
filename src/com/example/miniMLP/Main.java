35d930c6f854248179aebb4a5e1b5516e25c63a7
amory3113 <maksshevelenko@gmail.com>
2025-04-07 13:41:24 +0300

    init

--- src/com/example/miniMLP/Main.java
+++ src/com/example/miniMLP/Main.java
@@ -3,6 +3,7 @@ package com.example.miniMLP;
 import javax.swing.SwingUtilities;
 import com.example.miniMLP.ui.AppFrame;
 
+
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(AppFrame::new);
